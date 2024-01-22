d86c0362f02ff4e5a84375ef0f9ca5a43bcc7604
abhinavProgrammersUnion <tvabhinav008@gmail.com>
2024-01-22 14:51:17 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,22 @@
+/** @type {import('next').NextConfig} */
+const webpack = require('webpack');
+const nextConfig = {
+  reactStrictMode: false,
+  swcMinify:true,
+  unoptimized:false,
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.ProvidePlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+      })
+    );
+    return config;
+  },
+  
+};
+
+  
+
+module.exports = nextConfig;
